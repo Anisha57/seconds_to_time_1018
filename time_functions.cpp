5854bc1e99c4663bf7695c75cad9f512f9b5f842
Anisha57 <anisha.bhatta@g.austincc.edu>
2025-10-18 22:14:52 -0500

    Add implementation file time_functions.cpp

---
+++ time_functions.cpp
@@ -0,0 +1,10 @@
+#include "time_functions.h"
+int get_hours(int seconds_since_1970) {
+    return (seconds_since_1970 / 3600) % 24;
+}
+int get_minutes(int seconds_since_1970) {
+    return (seconds_since_1970 / 60) % 60;
+}
+int get_seconds(int seconds_since_1970) {
+    return seconds_since_1970 % 60;
+}
