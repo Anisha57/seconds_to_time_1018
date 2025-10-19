18b28772fd6fb817e9531eec33406533a529ba2c
Anisha57 <anisha.bhatta@g.austincc.edu>
2025-10-18 22:12:10 -0500

    file time_functions.h
    This header file provides declarations for functions that convert seconds since the Unix epoch into hours, minutes, and seconds.

---
+++ time_functions.h
@@ -0,0 +1,8 @@
+#ifndef TIME_FUNCTIONS_H
+#define TIME_FUNCTIONS_H
+
+int get_hours(int seconds_since_1970);
+int get_minutes(int seconds_since_1970);
+int get_seconds(int seconds_since_1970);
+
+#endif
